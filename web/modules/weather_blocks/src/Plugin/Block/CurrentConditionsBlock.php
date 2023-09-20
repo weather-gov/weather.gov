e9c041bdfcbf62a0de14869daed24a7203e49abb
Greg Walker <michael.walker@noaa.gov>
2023-09-20 14:54:32 -0500

    get from API

--- web/modules/weather_blocks/src/Plugin/Block/CurrentConditionsBlock.php
+++ web/modules/weather_blocks/src/Plugin/Block/CurrentConditionsBlock.php
@@ -3,9 +3,11 @@
 namespace Drupal\weather_blocks\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
- * Provides a 'Hello' Block.
+ * Provides a block of the current weather conditions.
  *
  * @Block(
  *   id = "weathergov_current_conditions",
@@ -13,8 +15,47 @@ use Drupal\Core\Block\BlockBase;
  *   category = @Translation("weather.gov"),
  * )
  */
-class CurrentConditionsBlock extends BlockBase {
+class CurrentConditionsBlock extends BlockBase implements ContainerFactoryPluginInterface {
 
+  /**
+   * A service for fetching weather data.
+   *
+   * @var WeatherDataService weatherData
+   */
+  private $weatherData;
+
+  /**
+   * Constructor for dependency injection.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, $weatherDataService) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->weatherData = $weatherDataService;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('weather_data')
+    );
+  }
+
+  /**
+   * Disable cacheing on this block.
+   *
+   * Because this is displayed to anonymous
+   * users and it is location-based (or will be), we can't really rely on any
+   * cacheing here right now.
+   *
+   * Once we hook up location data, it may be the case that Drupal can cache
+   * responses based on that location, in which case a short cache could work
+   * fine. Not 100% convinced we should do it, though, because we'd just be
+   * trading one kind of complexity for another (time vs. space).
+   */
   public function getCacheMaxAge() {
     return 0;
   }
@@ -23,29 +64,10 @@ class CurrentConditionsBlock extends BlockBase {
    * {@inheritdoc}
    */
   public function build() {
-    date_default_timezone_set('America/New_York');
-
     return [
       '#theme' => "weather_blocks_current_conditions",
-      '#data' => [
-        'conditions' => [
-          'long' => 'rain with high cloud cover',
-          'short' => 'Rain showers'
-        ],
-        'feels_like' => 83,
-        'humidity' => 55,
-        'icon' => 'showers_scattered_rain.svg',
-        'location' => 'Nashville, TN',
-        'temperature' => 79,
-        'timestamp' => [
-          'formatted' => date("l g:i A T"),
-          'utc' => time()
-        ],
-        'wind' => [
-          'speed' => 10,
-          'direction' => 'NE'
-        ]
-      ],
+      '#data' => $this->weatherData->getCurrentConditions(),
     ];
   }
+
 }
