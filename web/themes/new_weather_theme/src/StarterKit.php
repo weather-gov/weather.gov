d65051c1355934f9ff4b9de8942b54c90ad79c91
Greg Walker <michael.walker@noaa.gov>
2023-10-03 14:20:08 -0500

    I think this does it

--- web/themes/new_weather_theme/src/StarterKit.php
+++ web/themes/new_weather_theme/src/StarterKit.php
@@ -5,6 +5,9 @@ namespace Drupal\starterkit_theme;
 use Drupal\Component\Serialization\Yaml;
 use Drupal\Core\Theme\StarterKitInterface;
 
+/**
+ * {@inheritdoc}
+ */
 final class StarterKit implements StarterKitInterface {
 
   /**
