c23fd25b139098743fd92270772eacb23a2d1430
Claire Annan <claire.annan@noaa.gov>
2023-10-03 08:02:09 -0400

    Add compiled USWDS source files in new directories.

--- web/themes/new_weather_theme/gulpfile.js
+++ web/themes/new_weather_theme/gulpfile.js
@@ -2,11 +2,11 @@ const uswds = require("@uswds/compile");
 
 uswds.settings.version = 3;
 
-uswds.paths.dist.theme = "./sass";
-uswds.paths.dist.css = "./assets/uswds/css";
-uswds.paths.dist.img = "./assets/uswds/img";
-uswds.paths.dist.fonts = "./assets/uswds/fonts";
-uswds.paths.dist.js = "./assets/uswds/js";
+uswds.paths.dist.theme = "./assets/sass";
+uswds.paths.dist.css = "./assets/css";
+uswds.paths.dist.img = "./assets/images/uswds/";
+uswds.paths.dist.fonts = "./assets/fonts";
+uswds.paths.dist.js = "./assets/js";
 
 exports.compile = uswds.compile;
 exports.init = uswds.init;
