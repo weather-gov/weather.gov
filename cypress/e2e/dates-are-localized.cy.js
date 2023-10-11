48e5139407491fd287627e6281d48a3f5916c031
Greg Walker <michael.walker@noaa.gov>
2023-10-11 17:25:24 -0500

    start hooking up cypress

---
+++ cypress/e2e/dates-are-localized.cy.js
@@ -0,0 +1,37 @@
+const dayjs = require("dayjs");
+
+describe("main script", () => {
+  describe("formats the timestamp according to the browser's locale settings", () => {
+    // US English, Mexican Spanish, Puerto Rican Spanish, and US Spanish. Just
+    // some representative test cases. If these pass, others should too.
+    ["en-US", "es-MX", "es-PR", "es-US"].forEach((locale) => {
+      it(`for the ${locale} locale`, () => {
+        cy.visit("/", {
+          onBeforeLoad: (win) => {
+            Object.defineProperty(win.navigator, "languages", {
+              value: [locale],
+            });
+          },
+        });
+        cy.get("weather-timestamp").then((timestamps) => {
+          expect(timestamps.length).to.be.greaterThan(0);
+
+          for (const timestamp of timestamps) {
+            const utc = timestamp.getAttribute("data-utc");
+            const actual = timestamp.innerText;
+
+            const formatter = new Intl.DateTimeFormat(locale, {
+              weekday: "long",
+              hour: "numeric",
+              minute: "2-digit",
+              timeZoneName: "short",
+            });
+
+            const expected = formatter.format(dayjs.unix(utc).toDate());
+            expect(actual).to.equal(expected);
+          }
+        });
+      });
+    });
+  });
+});
