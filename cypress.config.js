0b2a6c0505ff1f6d50c4cdb6336de1a7245b63c4
Greg Walker <michael.walker@noaa.gov>
2023-10-16 16:46:38 -0500

    oop, just always output for gh I guess

--- cypress.config.js
+++ cypress.config.js
@@ -7,15 +7,10 @@ module.exports = defineConfig({
     setupNodeEvents: (on) => {
       on("task", {
         a11yViolation: ({ node }) => {
-          if (process.env.CI === "true") {
-            // eslint-disable-next-line no-console
-            console.log(
-              `::error Accessibility error.::${node.any[0].message} at ${node.html}`,
-            );
-          } else {
-            // eslint-disable-next-line no-console
-            console.log(node);
-          }
+          // eslint-disable-next-line no-console
+          console.log(
+            `::error Accessibility error.::${node.any[0].message} at ${node.html}`,
+          );
           return null;
         },
       });
