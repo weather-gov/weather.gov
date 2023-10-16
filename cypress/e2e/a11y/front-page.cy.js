0f419487a3ebb8a34c91a761c5501e448ad0bd32
Greg Walker <michael.walker@noaa.gov>
2023-10-16 16:32:07 -0500

    wcag2aa

--- cypress/e2e/a11y/front-page.cy.js
+++ cypress/e2e/a11y/front-page.cy.js
@@ -2,6 +2,7 @@ describe("front page", () => {
   it("passes automated accessibility checks", () => {
     cy.visit("/", {});
     cy.injectAxe();
+    cy.configureAxe({ runOnly: { values: ["wcag2aa"] } });
     cy.checkA11y(null, null, (violations) => {
       for (const violation of violations) {
         for (const node of violation.nodes) {
