1d90ed42d9c6f53cb6b99f147b1d2361ed03465b
Greg Walker <michael.walker@noaa.gov>
2023-10-16 15:00:55 -0500

    moop moop

--- cypress.config.js
+++ cypress.config.js
@@ -7,10 +7,15 @@ module.exports = defineConfig({
     setupNodeEvents: (on) => {
       on("task", {
         a11yViolation: ({ node }) => {
-          // eslint-disable-next-line no-console
-          console.log(
-            `::error Accessibility error.::${node.any[0].message} at ${node.html}`,
-          );
+          if (process.env.CI === "true") {
+            // eslint-disable-next-line no-console
+            console.log(
+              `::error Accessibility error.::${node.any[0].message} at ${node.html}`,
+            );
+          } else {
+            // eslint-disable-next-line no-console
+            console.log(node);
+          }
           return null;
         },
       });
