5a733ceb45c7b4ab689b0a707b871449479e6e91
Greg Walker <michael.walker@noaa.gov>
2024-01-12 16:21:43 -0600

    capture bug; add test; fix

--- tests/e2e/cypress.config.js
+++ tests/e2e/cypress.config.js
@@ -1,8 +1,15 @@
+const {
+  addMatchImageSnapshotPlugin,
+} = require("@simonsmith/cypress-image-snapshot/plugin");
+
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: "http://localhost:8080",
     screenshotOnRunFailure: false,
+    setupNodeEvents(on) {
+      addMatchImageSnapshotPlugin(on);
+    },
   },
 });
---
+++ tests/e2e/cypress/e2e/current-weather-narrative.cy.js
@@ -0,0 +1,19 @@
+describe("the current weather narrative", () => {
+  before(() => {
+    cy.request("http://localhost:8081/local");
+  });
+
+  it("is on its own line", () => {
+    cy.viewport(1020, 500);
+
+    cy.visit("/local/TST/20/20");
+    cy.get("html").invoke("css", "height", "initial");
+    cy.get("body").invoke("css", "height", "initial");
+
+    cy.get(".weather-gov-current-conditions weather-timestamp").then((node) => {
+      node.text("Friday, 12:12 PM EDT");
+    });
+
+    cy.matchImageSnapshot();
+  });
+});
