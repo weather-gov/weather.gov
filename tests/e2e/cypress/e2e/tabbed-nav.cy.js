5fc8aba1e0b4b29b2eee3fd46b01adfb8887336c
eric-gade <eric.gade@gsa.gov>
2024-02-01 13:02:34 -0500

    Formatting

--- tests/e2e/cypress/e2e/tabbed-nav.cy.js
+++ tests/e2e/cypress/e2e/tabbed-nav.cy.js
@@ -7,62 +7,53 @@ describe("<tabbed-nav> component tests", () => {
     describe("Basic tabbed nav tests", () => {
       let tabbedNav;
       it("Can find the tabbed-nav element on the page", () => {
-        tabbedNav = cy.get("tabbed-nav")
-          .then(element => {
-            tabbedNav = element;
-            expect(tabbedNav).to.exist;
-          });
+        tabbedNav = cy.get("tabbed-nav").then((element) => {
+          tabbedNav = element;
+          expect(tabbedNav).to.exist;
+        });
       });
 
       it("Knows the tabbed-nav is a defined custom element", () => {
-        cy.window()
-          .then(win => {
-            const customEl = win.customElements.get("tabbed-nav");
-            expect(customEl).to.exist;
-            expect(tabbedNav.get(0).isConnected).to.be.true;
-          });
+        cy.window().then((win) => {
+          const customEl = win.customElements.get("tabbed-nav");
+          expect(customEl).to.exist;
+          expect(tabbedNav.get(0).isConnected).to.be.true;
+        });
       });
 
       it("There is a default tab selected and its content is visible", () => {
-        cy
-          .get('tabbed-nav .tab-button[aria-expanded="true"]').as("selectedButton")
-          .should('exist')
+        cy.get('tabbed-nav .tab-button[aria-expanded="true"]')
+          .as("selectedButton")
+          .should("exist")
           .get("@selectedButton")
           .invoke("attr", "data-selected")
           .should("exist")
           .get("@selectedButton")
-          .then(btn => {
+          .then((btn) => {
             const tabName = btn.attr("data-tab-name");
-            cy
-              .get(`#${tabName}`)
-              .should("exist")
-              .should("be.visible");
+            cy.get(`#${tabName}`).should("exist").should("be.visible");
           });
       });
 
       it("The content of un-selected tabs should not be visible", () => {
-        cy
-          .get(`tabbed-nav .tab-button:not([data-selected])`).as("unselectedBtn")
+        cy.get(`tabbed-nav .tab-button:not([data-selected])`)
+          .as("unselectedBtn")
           .should("exist")
           .get("@unselectedBtn")
-          .then(btn => {
+          .then((btn) => {
             const tabName = btn.attr("data-tab-name");
-            cy
-              .get(`#${tabName}`)
-              .should("exist")
-              .should("not.be.visible");
+            cy.get(`#${tabName}`).should("exist").should("not.be.visible");
           });
       });
 
       it("Clicking an unselected tab should show it", () => {
-        cy
-          .get("tabbed-nav .tab-button:last").as("lastBtn")
+        cy.get("tabbed-nav .tab-button:last")
+          .as("lastBtn")
           .click()
           .get("@lastBtn")
-          .then(btn => {
+          .then((btn) => {
             const tabName = btn.attr("data-tab-name");
-            cy
-              .get(`#${tabName}`)
+            cy.get(`#${tabName}`)
               .should("be.visible")
               .invoke("attr", "data-selected")
               .should("exist");
@@ -77,59 +68,56 @@ describe("<tabbed-nav> component tests", () => {
 
       it("Clicking an unselected tab hides the other tabs", () => {
         cy.get("tabbed-nav .tab-button:last").click();
-        cy
-          .get("tabbed-nav .tab-button:not(:last)")
-          .each(btn => {
-            const tabName = btn.attr("data-tab-name");
-            cy
-              .get(`#${tabName}`).as("tabContent")
-              .should("not.be.visible")
-              .invoke("attr", "data-selected")
-              .should("not.exist");
-          });
+        cy.get("tabbed-nav .tab-button:not(:last)").each((btn) => {
+          const tabName = btn.attr("data-tab-name");
+          cy.get(`#${tabName}`)
+            .as("tabContent")
+            .should("not.be.visible")
+            .invoke("attr", "data-selected")
+            .should("not.exist");
+        });
       });
     });
 
     describe("Intercepts click events on above-the-fold alert links", () => {
       it("Clicking an alert link opens the accordion for that link and scrolls to it", () => {
-        cy
-          .get("weathergov-alert-list a")
-          .each(anchorEl => {
-            const alertId = anchorEl.attr("href").split("#")[1];
-            const alertEl = cy.get(`#${alertId}`).as("alertEl").click();
-            cy
-              .wait(20)
-              .get("@alertEl").find(".usa-accordion__content")
-              .invoke("attr", "hidden")
-              .should("not.exist")
-              .get("@alertEl")
-              .find("button.usa-accordion__button")
-              .invoke("attr", "aria-expanded")
-              .should("eq", "true")
-              .get("@alertEl")
-              .should("be.visible"); 
-          });
+        cy.get("weathergov-alert-list a").each((anchorEl) => {
+          const alertId = anchorEl.attr("href").split("#")[1];
+          const alertEl = cy.get(`#${alertId}`).as("alertEl").click();
+          cy.wait(20)
+            .get("@alertEl")
+            .find(".usa-accordion__content")
+            .invoke("attr", "hidden")
+            .should("not.exist")
+            .get("@alertEl")
+            .find("button.usa-accordion__button")
+            .invoke("attr", "aria-expanded")
+            .should("eq", "true")
+            .get("@alertEl")
+            .should("be.visible");
+        });
       });
 
       it("Opens the alerts tab (when not selected) when an alert link is clicked", () => {
         cy
-        // Click on another tab that is not the alerts
-        // tab button
+          // Click on another tab that is not the alerts
+          // tab button
           .get('tabbed-nav .tab-button:not([data-tab-name="alerts"]):first')
           .click()
-        // Get the third alert link and click it
+          // Get the third alert link and click it
           .get("weathergov-alert-list a:last")
           .click()
-        // Get the alerts tab button and make sure it's now
-        // selected
-          .get('tabbed-nav .tab-button[data-tab-name="alerts"]').as("alertsTabBtn")
+          // Get the alerts tab button and make sure it's now
+          // selected
+          .get('tabbed-nav .tab-button[data-tab-name="alerts"]')
+          .as("alertsTabBtn")
           .invoke("attr", "aria-expanded")
           .should("eq", "true")
           .get("@alertsTabBtn")
           .invoke("attr", "data-selected")
           .should("exist")
-        // Get the alerts tab content area and make sure
-        // it is showing
+          // Get the alerts tab content area and make sure
+          // it is showing
           .get("#alerts")
           .should("be.visible")
           .invoke("attr", "data-selected")
--- web/themes/new_weather_theme/assets/js/components/TabbedNavigator.js
+++ web/themes/new_weather_theme/assets/js/components/TabbedNavigator.js
@@ -3,158 +3,156 @@ const templateString = `
 `;
 
 class TabbedNavigator extends HTMLElement {
-    constructor(){
-        super();
+  constructor() {
+    super();
 
-        this.template = document.createElement('template');
-        this.template.innerHTML = templateString;
-        this.attachShadow({mode: 'open'});
-        this.shadowRoot.append(
-            this.template.content.cloneNode(true)
-        );
+    this.template = document.createElement("template");
+    this.template.innerHTML = templateString;
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.append(this.template.content.cloneNode(true));
 
-        // Bind this context to methods that need it
-        this.handleAlertAnchorClick = this.handleAlertAnchorClick.bind(this);
-        this.handleTabButtonClick = this.handleTabButtonClick.bind(this);
-        this.switchToTab = this.switchToTab.bind(this);
-        this.scrollToAccordion = this.scrollToAccordion.bind(this);
+    // Bind this context to methods that need it
+    this.handleAlertAnchorClick = this.handleAlertAnchorClick.bind(this);
+    this.handleTabButtonClick = this.handleTabButtonClick.bind(this);
+    this.switchToTab = this.switchToTab.bind(this);
+    this.scrollToAccordion = this.scrollToAccordion.bind(this);
+  }
+
+  connectedCallback() {
+    // If no tabs are selected by default, then select the first one
+    const selected = Array.from(
+      this.querySelectorAll(".tab-button[data-selected]"),
+    );
+    if (!selected.length) {
+      this.switchToTab(this.querySelector("button").dataset.tabName);
     }
-    
-    connectedCallback(){
-        // If no tabs are selected by default, then select the first one
-        const selected = Array.from(this.querySelectorAll('.tab-button[data-selected]'));
-        if(!selected.length){
-            this.switchToTab(this.querySelector('button').dataset.tabName);
-        }
 
-        // The initial page load might contain a hash fragment
-        // referring either to content within a given tab
-        // (such as an alert) or a tab itself. We should handle
-        // these two cases.
-        this.navigateWithInitialHash();
+    // The initial page load might contain a hash fragment
+    // referring either to content within a given tab
+    // (such as an alert) or a tab itself. We should handle
+    // these two cases.
+    this.navigateWithInitialHash();
 
-        // Intercept click events on Alert links at the
-        // top of the page and handle them in this component
-        Array.from(document.querySelectorAll('weathergov-alert-list a')).forEach(alertAnchor => {
-            alertAnchor.addEventListener('click', this.handleAlertAnchorClick);
+    // Intercept click events on Alert links at the
+    // top of the page and handle them in this component
+    Array.from(document.querySelectorAll("weathergov-alert-list a")).forEach(
+      (alertAnchor) => {
+        alertAnchor.addEventListener("click", this.handleAlertAnchorClick);
+      },
+    );
+
+    // Add needed event listeners
+    Array.from(this.querySelectorAll("button.tab-button")).forEach((button) => {
+      button.addEventListener("click", this.handleTabButtonClick);
+    });
+  }
+
+  disconnectedCallback() {
+    // Remove any event listeners
+    Array.from(this.querySelectorAll("button.tab-button")).forEach((button) => {
+      button.removeEventListener("click", this.handleTabButtonClick);
+    });
+  }
+
+  navigateWithInitialHash() {
+    const hash = new URL(window.location).hash;
+    if (!hash || hash == "") {
+      return;
+    }
+
+    const matchedTabButton = this.querySelector(
+      `[data-tab-name="${hash.replace("#", "")}"]`,
+    );
+    if (matchedTabButton) {
+      this.switchToTab(matchedTabButton.dataset.tabName);
+      matchedTabButton.parentElement.scrollIntoView();
+      return;
+    }
+
+    const childElement = this.querySelector(`${hash}`);
+    if (childElement) {
+      const tabContainer = childElement.closest(".tab-container");
+      this.switchToTab(tabContainer.id);
+      if (childElement.matches(".usa-accordion")) {
+        this.toggleAccordion(childElement, true);
+        document.addEventListener("DOMContentLoaded", () => {
+          this.scrollToAccordion(childElement);
         });
-
-        // Add needed event listeners
-        Array.from(this.querySelectorAll('button.tab-button')).forEach(button => {
-            button.addEventListener('click', this.handleTabButtonClick);
-        });
+        return;
+      }
     }
+  }
 
-    disconnectedCallback(){
-        // Remove any event listeners
-        Array.from(this.querySelectorAll('button.tab-button')).forEach(button => {
-            button.removeEventListener('click', this.handleTabButtonClick);
-        });
+  switchToTab(tabId) {
+    const activeElements = this.querySelectorAll("[data-selected]");
+    Array.from(activeElements).forEach((activeElement) => {
+      activeElement.removeAttribute("data-selected");
+      if (activeElement.hasAttribute("aria-expanded")) {
+        activeElement.setAttribute("aria-expanded", "false");
+      }
+    });
+    const tabButton = this.querySelector(`[data-tab-name="${tabId}"]`);
+    tabButton.setAttribute("data-selected", "");
+    tabButton.setAttribute("aria-expanded", "true");
+    const tabContainer = this.querySelector(`#${tabId}`);
+    tabContainer.setAttribute("data-selected", "");
+  }
+
+  handleTabButtonClick(event) {
+    this.switchToTab(event.target.dataset.tabName);
+    // Since this was an actual click, update the hash
+    // of the site to the tab button's id
+    window.history.replaceState(null, null, `#${event.target.dataset.tabName}`);
+  }
+
+  handleAlertAnchorClick(event) {
+    const hash = new URL(event.target.href).hash;
+    const accordionEl = this.querySelector(`${hash}.usa-accordion`);
+    if (accordionEl) {
+      // If we get here, then the element referred
+      // to by the href is a child of this tabbed
+      // navigator.
+      // We need to toggle to the correct tab pane
+      // to properly display and scroll to the element.
+      const tabContainer = accordionEl.closest(".tab-container");
+      this.switchToTab(tabContainer.id);
+      this.toggleAccordion(accordionEl, true);
+
+      // Because we use a sticky position on
+      // the tab button area, the normal browser
+      // scrolling will not display the proper position
+      // to the user. Instead, we have to roll our
+      // own scrolling method
+      this.scrollToAccordion(accordionEl);
+      event.preventDefault();
+      window.history.replaceState(null, null, hash);
     }
+  }
 
-    navigateWithInitialHash(){
-        const hash = new URL(window.location).hash;
-        if(!hash || hash == ""){
-            return;
-        }
-        
-        const matchedTabButton = this.querySelector(`[data-tab-name="${hash.replace("#", "")}"]`);
-        if(matchedTabButton){
-            this.switchToTab(matchedTabButton.dataset.tabName);
-            matchedTabButton.parentElement.scrollIntoView();
-            return;
-        }
+  toggleAccordion(accordionElement, on = true) {
+    const button = accordionElement.querySelector(
+      "button.usa-accordion__button",
+    );
+    const content = accordionElement.querySelector(".usa-accordion__content");
 
-        const childElement = this.querySelector(`${hash}`);
-        if(childElement){
-            const tabContainer = childElement.closest('.tab-container');
-            this.switchToTab(tabContainer.id);
-            if(childElement.matches('.usa-accordion')){
-                this.toggleAccordion(childElement, true);
-                document.addEventListener('DOMContentLoaded', () => {
-                    this.scrollToAccordion(childElement);
-                })
-                return;
-            }
-        }
+    if (on) {
+      button.setAttribute("aria-expanded", "true");
+      content.removeAttribute("hidden");
+    } else {
+      button.setAttribute("aria-expanded", "false");
+      content.addAttribute("hidden", "");
     }
+  }
 
-    switchToTab(tabId){
-        const activeElements = this.querySelectorAll('[data-selected]');
-        Array.from(activeElements).forEach(activeElement => {
-            activeElement.removeAttribute('data-selected');
-            if(activeElement.hasAttribute('aria-expanded')){
-                activeElement.setAttribute('aria-expanded', 'false');
-            }
-        });
-        const tabButton = this.querySelector(`[data-tab-name="${tabId}"]`);
-        tabButton.setAttribute('data-selected', '');
-        tabButton.setAttribute('aria-expanded', 'true');
-        const tabContainer = this.querySelector(`#${tabId}`);
-        tabContainer.setAttribute('data-selected', '');
-    }
-
-    handleTabButtonClick(event){
-        this.switchToTab(event.target.dataset.tabName);
-        // Since this was an actual click, update the hash
-        // of the site to the tab button's id
-        window.history.replaceState(
-            null,
-            null,
-            `#${event.target.dataset.tabName}`
-        );
-    }
-
-    handleAlertAnchorClick(event){
-        const hash = new URL(event.target.href).hash;
-        const accordionEl = this.querySelector(`${hash}.usa-accordion`);
-        if(accordionEl){
-            // If we get here, then the element referred
-            // to by the href is a child of this tabbed
-            // navigator.
-            // We need to toggle to the correct tab pane
-            // to properly display and scroll to the element.
-            const tabContainer = accordionEl.closest('.tab-container');
-            this.switchToTab(tabContainer.id);
-            this.toggleAccordion(accordionEl, true);
-
-            // Because we use a sticky position on
-            // the tab button area, the normal browser
-            // scrolling will not display the proper position
-            // to the user. Instead, we have to roll our
-            // own scrolling method
-            this.scrollToAccordion(accordionEl);
-            event.preventDefault();
-            window.history.replaceState(
-                null,
-                null,
-                hash
-            );
-        }
-    }
-
-    toggleAccordion(accordionElement, on = true){
-        const button = accordionElement.querySelector('button.usa-accordion__button');
-        const content = accordionElement.querySelector('.usa-accordion__content');
-
-        if(on){
-            button.setAttribute('aria-expanded', 'true');
-            content.removeAttribute('hidden');
-        } else {
-            button.setAttribute('aria-expanded', 'false');
-            content.addAttribute('hidden', '');
-        }
-    }
-
-    scrollToAccordion(accordionElement){
-        const accordionTop = accordionElement.getBoundingClientRect().top;
-        const buttonArea = this.querySelector('.tab-buttons');
-        const buttonAreaHeight = buttonArea.getBoundingClientRect().height;
-        const scrollY = (accordionTop + window.scrollY) - buttonAreaHeight;
-        console.log(scrollY);
-        window.scrollTo(0, scrollY);
-    }
+  scrollToAccordion(accordionElement) {
+    const accordionTop = accordionElement.getBoundingClientRect().top;
+    const buttonArea = this.querySelector(".tab-buttons");
+    const buttonAreaHeight = buttonArea.getBoundingClientRect().height;
+    const scrollY = accordionTop + window.scrollY - buttonAreaHeight;
+    console.log(scrollY);
+    window.scrollTo(0, scrollY);
+  }
 }
 
-window.customElements.define('tabbed-nav', TabbedNavigator);
+window.customElements.define("tabbed-nav", TabbedNavigator);
 console.log("Loaded TabbedNavigator here");
