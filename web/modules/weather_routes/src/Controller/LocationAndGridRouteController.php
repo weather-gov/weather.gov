55b6be2e1a55f41a18b164390b5bce405a5a8971
Greg Walker <michael.walker@noaa.gov>
2024-02-20 16:16:11 -0600

    oop, clean up

--- web/modules/weather_routes/src/Controller/LocationAndGridRouteController.php
+++ web/modules/weather_routes/src/Controller/LocationAndGridRouteController.php
@@ -70,36 +70,21 @@ final class LocationAndGridRouteController extends ControllerBase
      */
     public function redirectFromGrid($wfo, $gridX, $gridY)
     {
-        $geometry = $this->weatherData->getGeometryFromGrid(
-            $wfo,
-            $gridX,
-            $gridY,
-        );
-        $point = $geometry[0];
+        try {
+            $geometry = $this->weatherData->getGeometryFromGrid(
+                $wfo,
+                $gridX,
+                $gridY,
+            );
+            $point = $geometry[0];
 
-        $url = Url::fromRoute("weather_routes.point", [
-            "lat" => round($point->lat, 4),
-            "lon" => round($point->lon, 4),
-        ]);
-        return new RedirectResponse($url->toString());
-
-        return new NotFoundHttpException();
-
-        $grid = $this->weatherData->getGridFromLatLon($lat, $lon);
-
-        if ($grid == null) {
-            // If we don't get a corresponding grid location, throw a 404.
+            $url = Url::fromRoute("weather_routes.point", [
+                "lat" => round($point->lat, 4),
+                "lon" => round($point->lon, 4),
+            ]);
+            return new RedirectResponse($url->toString());
+        } catch (\Throwable $e) {
             throw new NotFoundHttpException();
         }
-
-        if ($grid["wfo"] == null) {
-            $logger = $this->getLogger("Weather.gov data service");
-            $logger->notice("location has no grid: $lat / $lon");
-
-            return new RedirectResponse("/not-implemented");
-        }
-
-        $url = Url::fromRoute("weather_routes.grid", $grid);
-        return new RedirectResponse($url->toString());
     }
 }
