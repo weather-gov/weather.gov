f81215bb9384ce65e4a7c9414586e03e7ba49fb3
eric-gade <eric.gade@gsa.gov>
2024-02-22 15:51:11 -0500

    Removing shadow dom and updating scroll button icons/styles

--- web/themes/new_weather_theme/assets/js/components/HourlyTable.js
+++ web/themes/new_weather_theme/assets/js/components/HourlyTable.js
@@ -1,90 +1,47 @@
-const hourlyTableTemplateString = `
-<style>
-:host {
-display: block;
-position: relative;
-}
-.scroll-arrow {
-display: block;
-width: 40px;
-height: 40px;
-background-color: #A9AEB1;
-border-radius: 100%;
-position: absolute;
-top: -20px;
-z-index: 1000;
-}
-
-#arrow-left {
-left: 16px;
-}
-
-#arrow-right {
-right: 16px;
-}
-
-#table-wrapper {
-display: block;
-overflow-y: hidden;
-}
-</style>
-<div id="arrow-left" class="scroll-arrow"></div>
-<div id="arrow-right" class="scroll-arrow"></div>
-<div id="table-wrapper"/>
-<slot></slot>
-</div>
-`;
-
 class HourlyTable extends HTMLElement {
   constructor(){
     super();
-
-    this.template = document.createElement("template");
-    this.template.innerHTML = hourlyTableTemplateString;
-    this.attachShadow({mode: "open"});
-    this.shadowRoot.append(
-      this.template.content.cloneNode(true)
-    );
 
     // Bound component methods
     this.handleArrowButtonClick = this.handleArrowButtonClick.bind(this);
   }
 
   connectedCallback(){
-    Array.from(this.shadowRoot.querySelectorAll(".scroll-arrow")).forEach(arrowButton => {
+    Array.from(this.querySelectorAll(".wx-scroll-button")).forEach(arrowButton => {
       arrowButton.addEventListener("click", this.handleArrowButtonClick);
     });
   }
 
   disconnectedCallback(){
-    Array.from(this.shadowRoot.querySelectorAll(".scroll-arrow")).forEach(arrowButton => {
+    Array.from(this.querySelectorAll(".wx-scroll-button")).forEach(arrowButton => {
       arrowButton.removeEventListener("click", this.handleArrowButtonClick);
     });
   }
 
   handleArrowButtonClick(event){
-    const wrapper = this.shadowRoot.getElementById("table-wrapper");
+    const wrapper = this.querySelector(".usa-table-container--scrollable");
+    console.log(event.currentTarget.dataset.direction);
 
-    if(event.target.id === "arrow-right"){
+    if(event.currentTarget.dataset.direction === "right"){
       this.clickScrollRight(wrapper);
-    } else if(event.target.id === "arrow-left" ){
+    } else if(event.currentTarget.dataset.direction === "left" ){
       this.clickScrollLeft(wrapper);
     }
   }
 
   clickScrollRight(wrapper){
-    const cellsToRight = Array.from(this.querySelectorAll("table tr:nth-child(2) td")).filter(cell => {
+    const cellsToRight = Array.from(this.querySelectorAll("table tr:nth-child(3) td")).filter(cell => {
       const diff = (cell.offsetLeft - wrapper.scrollLeft);
       return diff > 16;
     });
-    
+
     if(cellsToRight.length){
       wrapper.scrollTo({left: cellsToRight[0].offsetLeft - 16, behavior: "smooth"});
     }
   }
 
   clickScrollLeft(wrapper){
-    const cellsToLeft = Array.from(this.querySelectorAll("table tr:nth-child(2) td")).filter(cell => {
+    const cellsToLeft = Array.from(this.querySelectorAll("table tr:nth-child(3) td")).filter(cell => {
       const diff = cell.offsetLeft - wrapper.scrollLeft;
       return diff < 16;
     });
