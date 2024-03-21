08fcd9c140b8f3d1db77fe46991e68e45da00d20
eric-gade <eric.gade@gsa.gov>
2024-03-21 14:10:14 -0400

    Initial commit of experiments

--- web/themes/new_weather_theme/assets/js/locationSearch.js
+++ web/themes/new_weather_theme/assets/js/locationSearch.js
@@ -1,3 +1,231 @@
+// The number of milliseconds to delay
+// before triggering the field's input
+// handler. This prevents us from triggering
+// requests for every keystroke when the user
+// is typing rapidly.
+const INPUT_WAIT_MS = 100;
+
+class LocationSearchWrapper {
+  constructor(){
+    this._timeout = null;
+
+    // Bind instance methods
+    this.handleLocationSelected = this.handleLocationSelected.bind(this);
+    this.enqueueSearchChanged = this.enqueueSearchChanged.bind(this);
+    this.handleSearchChanged = this.handleSearchChanged.bind(this);
+    this.goToLocationPage = this.goToLocationPage.bind(this);
+
+    // Bind the appropriate event handlers to
+    // the wrapped element
+    document.addEventListener("input", this.enqueueSearchChanged);
+    document.addEventListener("change", this.handleLocationSelected);
+  }
+
+  async handleLocationSelected (event){
+    // We only care about change events that have
+    // come from select elements with the "weather location"
+    // name.
+    // Note that we cannot use the id here, because USWDS strips it,
+    // giving the input element it inserts that id value instead
+    if (!event.target.getAttribute("name") === "weather location") {
+      return;
+    }
+    const { value } = event.target;
+    const url = `https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/find?magicKey=${value}&f=json&_=1695666335115`;
+
+    const response = await fetch(url, {
+      headers: {
+        'Content-Type': "application/json"
+      }
+    });
+
+    const results = await response.json();
+
+    if (
+      !results.error &&
+      Array.isArray(results.locations) &&
+      results.locations.length > 0
+    ) {
+      const {
+        locations: [
+          {
+            feature: { geometry },
+          },
+        ],
+      } = results;
+
+      const lat = Math.round(geometry.y * 1_000) / 1_000;
+      const lon = Math.round(geometry.x * 1_000) / 1_000;
+
+      const placeName = event.target.selectedOptions[0].innerText;
+
+      this.goToLocationPage(lat, lon, placeName);
+    }
+  }
+
+  enqueueSearchChanged(event){
+    if(this._timeout){
+      window.clearTimeout(this._timeout);
+    }
+    this._timeout = window.setTimeout(() => {
+      this.handleSearchChanged(event);
+    }, INPUT_WAIT_MS);
+  }
+
+  async handleSearchChanged(event){
+    // We only care about input elements with the
+    // id that we've specified in the template.
+    if (event.target.id !== "weather-location-search") {
+      return;
+    }
+    const { value } = event.target;
+
+    // The select element is a sibling to this input.
+    const selectElement = event.target.parentElement.querySelector("select");
+
+    if (value.length >= 3) {
+      const url = `https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/suggest?f=json&countryCode=USA%2CPRI%2CVIR%2CGUM%2CASM&category=Land+Features%2CBay%2CChannel%2CCove%2CDam%2CDelta%2CGulf%2CLagoon%2CLake%2COcean%2CReef%2CReservoir%2CSea%2CSound%2CStrait%2CWaterfall%2CWharf%2CAmusement+Park%2CHistorical+Monument%2CLandmark%2CTourist+Attraction%2CZoo%2CCollege%2CBeach%2CCampground%2CGolf+Course%2CHarbor%2CNature+Reserve%2COther+Parks+and+Outdoors%2CPark%2CRacetrack%2CScenic+Overlook%2CSki+Resort%2CSports+Center%2CSports+Field%2CWildlife+Reserve%2CAirport%2CFerry%2CMarina%2CPier%2CPort%2CResort%2CPostal%2CPopulated+Place&maxSuggestions=10&_=1695666335097&text=${value}`;
+      const response = await fetch(url, {headers: {'Content-Type': 'application/json'}});
+      const { suggestions } = await response.json();
+
+      selectElement.innerText = "";
+
+      suggestions.forEach(({ text, magicKey }) => {
+        const option = document.createElement("option");
+        option.innerText = text;
+        option.setAttribute("value", magicKey);
+        selectElement.append(option);
+      });
+    }
+  }
+
+  goToLocationPage(latitude, longitude, placename){
+    // We want to redirect the user via a POST. We already have the form ready to
+    // go, we just need to set its action so the browser knows where to go.
+    const form = document.querySelector("form[data-location-search]");
+    form.setAttribute("action", `/point/${latitude}/${longitude}`);
+
+    // If we also have a suggested place name, add that to the form.
+    if (placename) {
+      const suggestion = form.querySelector(`input[name="suggestedPlaceName"]`);
+      suggestion.setAttribute("value", placename);
+    }
+
+    form.submit();
+  }
+}
+
+const LocationSearchWrapper2 = {
+  _timeout: null,
+  enqueueSearchChanged: (event) => {
+    if(this._timeout){
+      window.clearTimeout(this._timeout);
+    }
+    this._timeout = window.setTimeout(() => {
+      this.handleSearchChanged(event);
+    }, INPUT_WAIT_MS);
+  },
+  handleLocationSelected: async (event) => {
+    // We only care about change events that have
+    // come from select elements with the "weather location"
+    // name.
+    // Note that we cannot use the id here, because USWDS strips it,
+    // giving the input element it inserts that id value instead
+    if (!event.target.getAttribute("name") === "weather location") {
+      return;
+    }
+    const { value } = event.target;
+    const url = `https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/find?magicKey=${value}&f=json&_=1695666335115`;
+
+    const response = await fetch(url, {
+      headers: {
+        'Content-Type': "application/json"
+      }
+    });
+
+    const results = await response.json();
+
+    if (
+      !results.error &&
+      Array.isArray(results.locations) &&
+      results.locations.length > 0
+    ) {
+      const {
+        locations: [
+          {
+            feature: { geometry },
+          },
+        ],
+      } = results;
+
+      const lat = Math.round(geometry.y * 1_000) / 1_000;
+      const lon = Math.round(geometry.x * 1_000) / 1_000;
+
+      const placeName = event.target.selectedOptions[0].innerText;
+
+      this.goToLocationPage(lat, lon, placeName);
+    }
+  },
+
+  handleSearchChanged: async (event) => {
+    // We only care about input elements with the
+    // id that we've specified in the template.
+    if (event.target.id !== "weather-location-search") {
+      return;
+    }
+    const { value } = event.target;
+
+    // The select element is a sibling to this input.
+    const selectElement = event.target.parentElement.querySelector("select");
+
+    if (value.length >= 3) {
+      const url = `https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/suggest?f=json&countryCode=USA%2CPRI%2CVIR%2CGUM%2CASM&category=Land+Features%2CBay%2CChannel%2CCove%2CDam%2CDelta%2CGulf%2CLagoon%2CLake%2COcean%2CReef%2CReservoir%2CSea%2CSound%2CStrait%2CWaterfall%2CWharf%2CAmusement+Park%2CHistorical+Monument%2CLandmark%2CTourist+Attraction%2CZoo%2CCollege%2CBeach%2CCampground%2CGolf+Course%2CHarbor%2CNature+Reserve%2COther+Parks+and+Outdoors%2CPark%2CRacetrack%2CScenic+Overlook%2CSki+Resort%2CSports+Center%2CSports+Field%2CWildlife+Reserve%2CAirport%2CFerry%2CMarina%2CPier%2CPort%2CResort%2CPostal%2CPopulated+Place&maxSuggestions=10&_=1695666335097&text=${value}`;
+      const response = await fetch(url, {headers: {'Content-Type': 'application/json'}});
+      const { suggestions } = await response.json();
+
+      selectElement.innerText = "";
+
+      suggestions.forEach(({ text, magicKey }) => {
+        const option = document.createElement("option");
+        option.innerText = text;
+        option.setAttribute("value", magicKey);
+        selectElement.append(option);
+      });
+    }
+  },
+
+  goToLocationPage: (latitude, longitude, placename) => {
+    // We want to redirect the user via a POST. We already have the form ready to
+    // go, we just need to set its action so the browser knows where to go.
+    const form = document.querySelector("form[data-location-search]");
+    form.setAttribute("action", `/point/${latitude}/${longitude}`);
+
+    // If we also have a suggested place name, add that to the form.
+    if (placename) {
+      const suggestion = form.querySelector(`input[name="suggestedPlaceName"]`);
+      suggestion.setAttribute("value", placename);
+    }
+
+    form.submit();
+  },
+  start: () => {
+    // USWDS inserts an <input> element into the combo-box,
+    // but we do not have control over when that happens.
+    // This prevents us from reliably adding event listeners to
+    // that element.
+    // Instead, we wait for the events we care about to bubble
+    // up to the document and filter by attributes, ensuring
+    // we act on the location input combo box only.
+    document.addEventListener("input", this.enqueueSearchChanged);
+    document.addEventListener("change", this.handleLocationSelected);
+    console.log(this);
+  },
+  stop: () => {
+    document.removeEventListener("input", this.enqueueSearchChanged);
+    document.removeEventListener("change", this.handleLocationSelected);
+  }
+};
+
 (() => {
   const goToLocationPage = (latitude, longitude, placename) => {
     // We want to redirect the user via a POST. We already have the form ready to
@@ -131,104 +359,6 @@
       }
     });
   };
-
-  const createOneTimeJSONP = () => {
-    const name = `weathergov_${[...Array(30)]
-      .map(() => Math.round(Math.random() * 36).toString(36))
-      .join("")}`;
-
-    const load = (url) =>
-      new Promise((resolve) => {
-        const script = document.createElement("script");
-        script.setAttribute("src", url);
-
-        const jsonp = async (data) => {
-          script.remove();
-          delete window[name];
-
-          resolve(data);
-        };
-
-        window[name] = jsonp;
-        document.body.append(script);
-      });
-
-    return { load, name };
-  };
-
-  const locationSelected = async (event) => {
-    // We only care about change events that have
-    // come from select elements with the "weather location"
-    // name.
-    // Note that we cannot use the id here, because USWDS strips it,
-    // giving the input element it inserts that id value instead
-    if (!event.target.getAttribute("name") === "weather location") {
-      return;
-    }
-    const { value } = event.target;
-    const { load, name } = createOneTimeJSONP();
-    const url = `https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/find?callback=${name}&magicKey=${value}&f=json&_=1695666335115`;
-
-    const results = await load(url);
-
-    if (
-      !results.error &&
-      Array.isArray(results.locations) &&
-      results.locations.length > 0
-    ) {
-      const {
-        locations: [
-          {
-            feature: { geometry },
-          },
-        ],
-      } = results;
-
-      const lat = Math.round(geometry.y * 1_000) / 1_000;
-      const lon = Math.round(geometry.x * 1_000) / 1_000;
-
-      const placeName = event.target.selectedOptions[0].innerText;
-
-      goToLocationPage(lat, lon, placeName);
-    }
-  };
-
-  const searchChanged = async (event) => {
-    // We only care about input elements with the
-    // id that we've specified in the template.
-    if (event.target.id !== "weather-location-search") {
-      return;
-    }
-    const { value } = event.target;
-
-    // The select element is a sibling to this input.
-    const selectElement = event.target.parentElement.querySelector("select");
-
-    if (value.length >= 3) {
-      const { load, name } = createOneTimeJSONP();
-      const url = `https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/suggest?f=json&countryCode=USA%2CPRI%2CVIR%2CGUM%2CASM&category=Land+Features%2CBay%2CChannel%2CCove%2CDam%2CDelta%2CGulf%2CLagoon%2CLake%2COcean%2CReef%2CReservoir%2CSea%2CSound%2CStrait%2CWaterfall%2CWharf%2CAmusement+Park%2CHistorical+Monument%2CLandmark%2CTourist+Attraction%2CZoo%2CCollege%2CBeach%2CCampground%2CGolf+Course%2CHarbor%2CNature+Reserve%2COther+Parks+and+Outdoors%2CPark%2CRacetrack%2CScenic+Overlook%2CSki+Resort%2CSports+Center%2CSports+Field%2CWildlife+Reserve%2CAirport%2CFerry%2CMarina%2CPier%2CPort%2CResort%2CPostal%2CPopulated+Place&maxSuggestions=10&_=1695666335097&text=${value}&callback=${name}`;
-      const { suggestions } = await load(url);
-
-      selectElement.innerText = "";
-
-      suggestions.forEach(({ text, magicKey }) => {
-        const option = document.createElement("option");
-        option.innerText = text;
-        option.setAttribute("value", magicKey);
-        selectElement.append(option);
-      });
-    }
-  };
-
-  // USWDS inserts an <input> element into the combo-box,
-  // but we do not have control over when that happens.
-  // This prevents us from reliably adding event listeners to
-  // that element.
-  // Instead, we wait for the events we care about to bubble
-  // up to the document and filter by attributes, ensuring
-  // we act on the location input combo box only.
-  document.addEventListener("input", searchChanged);
-  document.addEventListener("change", locationSelected);
-
+  LocationSearchWrapper2.start();
   setupBrowserGeolocation();
 })();
