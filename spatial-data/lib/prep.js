81496055f81255df707c42d84beecd8c693e9a36
Greg Walker <michael.walker@noaa.gov>
2024-05-07 18:11:03 -0500

    test zip file before unzipping; if corrupt, retry download

--- spatial-data/lib/prep.js
+++ spatial-data/lib/prep.js
@@ -1,6 +1,17 @@
-const { exec } = require("node:child_process");
+const { exec: nodeExec } = require("node:child_process");
 const fs = require("node:fs/promises");
 const { fileExists } = require("./util.js");
+
+const exec = async (...args) =>
+  new Promise((resolve, reject) => {
+    nodeExec(...args, (err, stdout) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(stdout);
+      }
+    });
+  });
 
 module.exports.downloadAndUnzip = async (url) => {
   const filename = url.split("/").pop();
@@ -15,15 +26,20 @@ module.exports.downloadAndUnzip = async (url) => {
     console.log(`${filename} is already present`);
   }
 
-  await module.exports.unzip(filename);
+  await exec(`unzip -t ${filename}`)
+    .then(async () => {
+      await module.exports.unzip(filename);
 
-  console.log(`   [${filename}] done`);
+      console.log(`   [${filename}] done`);
+    })
+    .catch(async () => {
+      console.log("zip file is corrupt. Trying again...");
+      await fs.unlink(filename);
+      await module.exports.downloadAndUnzip(url);
+    });
 };
 
-module.exports.unzip = (path) =>
-  new Promise((resolve) => {
-    console.log(`   [${path}] decompressing...`);
-    exec(`unzip -u ${path}`, () => {
-      resolve();
-    });
-  });
+module.exports.unzip = async (path) => {
+  console.log(`   [${path}] decompressing...`);
+  await exec(`unzip -u ${path}`);
+};
