a78aecb14d6b390d05fcc1fe5aab6c05ecab4f92
Greg Walker <michael.walker@noaa.gov>
2024-06-24 17:02:48 -0500

    hide nav loader on page show

--- web/themes/new_weather_theme/assets/js/locationSearch.js
+++ web/themes/new_weather_theme/assets/js/locationSearch.js
@@ -1,4 +1,23 @@
 (() => {
+  const hideNavigationLoader = () => {
+    const loader = document.querySelector("wx-loader");
+    if (loader) {
+      loader.classList.add("display-none");
+    }
+  };
+
+  // When the page is shown, hide the navigation loader. This is the best we can
+  // do to handle hiding it when the back button is clicked. This code is not
+  // (necessarily) executed again when the back button is pressed. Instead, its
+  // state may simply be popped from the history stack. The pageshow event is
+  // fired any time this page is shown, regardless of source, and our event
+  // listener would persist as part of the popped state. The downside is there's
+  // a flash of the loader before it gets hidden.
+  //
+  // In the event that the browser has already unloaded this page's state from
+  // the history stack (to recoup memory, for example), then the page will
+  // reload entirely. In that case, the loader is already hidden.
+  window.addEventListener("pageshow", hideNavigationLoader);
 
   /**
    * If there is a navigation loader available on the page,
