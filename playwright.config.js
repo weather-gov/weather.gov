534b5340c21412ef26a08618979920b31e386e0d
eric-gade <eric.gade@gsa.gov>
2024-07-16 12:46:16 -0400

    Adding lmm's solutions

--- playwright.config.js
+++ playwright.config.js
@@ -19,7 +19,7 @@ const config = {
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI ? "github" : "line",
+  reporter: process.env.CI ? 'blob' : 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
