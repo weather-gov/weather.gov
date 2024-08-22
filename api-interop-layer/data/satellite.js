51be72d9eb4e30b6208e4a7399f0a1c6280c6e4b
Greg Walker <michael.walker@noaa.gov>
2024-08-21 19:25:40 -0500

    safely fail satellite

--- api-interop-layer/data/satellite.js
+++ api-interop-layer/data/satellite.js
@@ -1,14 +1,21 @@
 export default async ({ grid: { wfo } }) => {
-  const satelliteMetadata = await fetch(
-    `https://cdn.star.nesdis.noaa.gov/WFO/catalogs/WFO_02_${wfo.toLowerCase()}_catalog.json`,
-  ).then((r) => r.json());
+  try {
+    const satelliteMetadata = await fetch(
+      `https://cdn.star.nesdis.noaa.gov/WFO/catalogs/WFO_02_${wfo.toLowerCase()}_catalog.json`,
+    ).then((r) => r.json());
 
-  const satellite = satelliteMetadata.meta.satellite;
-  if (satellite) {
-    const goes = satellite === "GOES-West" ? "GOES18" : "GOES16";
+    const satellite = satelliteMetadata.meta.satellite;
+    if (satellite) {
+      const goes = satellite === "GOES-West" ? "GOES18" : "GOES16";
 
-    return {
-      gif: `https://cdn.star.nesdis.noaa.gov/WFO/${wfo.toLowerCase()}/GEOCOLOR/${goes}-${wfo.toUpperCase()}-GEOCOLOR-600x600.gif`,
-    };
+      return {
+        gif: `https://cdn.star.nesdis.noaa.gov/WFO/${wfo.toLowerCase()}/GEOCOLOR/${goes}-${wfo.toUpperCase()}-GEOCOLOR-600x600.gif`,
+      };
+    }
+  } catch (e) {
+    console.log(`Error getting satellite metadata for ${wfo}`);
+    console.log(e.message);
   }
+
+  return {};
 };
