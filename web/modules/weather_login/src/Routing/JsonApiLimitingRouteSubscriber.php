b26322d7d104d11bd4c575660f4d46f78175d657
James Tranovich <james.tranovich@noaa.gov>
2024-08-28 17:41:57 -0700

    php formatting

--- web/modules/weather_login/src/Routing/JsonApiLimitingRouteSubscriber.php
+++ web/modules/weather_login/src/Routing/JsonApiLimitingRouteSubscriber.php
@@ -5,11 +5,13 @@ namespace Drupal\weather_login\Routing;
 use Drupal\Core\Routing\RouteSubscriberBase;
 use Symfony\Component\Routing\RouteCollection;
 
-class JsonApiLimitingRouteSubscriber extends RouteSubscriberBase {
+class JsonApiLimitingRouteSubscriber extends RouteSubscriberBase
+{
     /**
      * {@inheritdoc}
      */
-    protected function alterRoutes(RouteCollection $collection) {
+    protected function alterRoutes(RouteCollection $collection)
+    {
         foreach ($collection as $name => $route) {
             $defaults = $route->getDefaults();
             if (!empty($defaults['_is_jsonapi'])) {
