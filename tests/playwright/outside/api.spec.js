79bfa695713911b8e647128b3384c9c7e6f235a9
James Tranovich <james.tranovich@noaa.gov>
2024-08-30 11:45:55 -0700

    outside testing: fix tests so they pass

--- tests/playwright/outside/api.spec.js
+++ tests/playwright/outside/api.spec.js
@@ -1,45 +1,55 @@
 const { test, expect } = require("@playwright/test");
 
-const { describe, beforeEach } = test;
+const { describe } = test;
 
-const API_ENDPOINT = "http://localhost:9080/jsonapi"
+const API_ENDPOINT = "http://localhost:9080/jsonapi";
 
 describe("API tests", () => {
-  test("users without credentials cannot get an API listing", async ({request}) => {
-    const response = await request.get(API_ENDPOINT);
-    expect(response.ok()).not.toBeTruthy();
-    const json = await response.json()
-    expect(json).toHaveProperty("errors");
-    expect(json).toMatchObject({"errors": [{
-      detail: "No authentication credentials provided.",
-      title: "Unauthorized",
-      status: "401",
-    }]});
-  });
-
-  test("anonymous users cannot get an API listing", async ({request}) => {
-    const response = await request.get(API_ENDPOINT, {
-      headers: {
-        'Authorization': `Basic ${'noop:noop'.toString('base64')}`,
-      }
+    test("users without credentials cannot get an API listing", async ({
+        request,
+    }) => {
+        const response = await request.get(API_ENDPOINT);
+        expect(response.ok()).not.toBeTruthy();
+        const json = await response.json();
+        expect(json).toHaveProperty("errors");
+        expect(json).toMatchObject({
+            errors: [
+                {
+                    detail: "No authentication credentials provided.",
+                    title: "Unauthorized",
+                    status: "401",
+                },
+            ],
+        });
     });
-    expect(response.ok()).not.toBeTruthy();
-    const json = await response.json()
-    expect(json).toHaveProperty("errors");
-    expect(json).toMatchObject({"errors": [{
-      title: "Forbidden",
-      status: "403",
-    }]});
-  });
 
-  test("uploader can see the API listing", async({request}) => {
-    const response = await request.get(API_ENDPOINT, {
-      headers: {
-        'Authorization': `Basic ${'testuser:testpass'.toString('base64')}`,
-      }
+    test("anonymous users cannot get an API listing", async ({ request }) => {
+        const response = await request.get(API_ENDPOINT, {
+            headers: {
+                Authorization: `Basic ${btoa("foo:foo")}`,
+            },
+        });
+        expect(response.ok()).not.toBeTruthy();
+        const json = await response.json();
+        expect(json).toHaveProperty("errors");
+        expect(json).toMatchObject({
+            errors: [
+                {
+                    title: "Forbidden",
+                    status: "403",
+                },
+            ],
+        });
     });
-    expect(response.ok()).toBeTruthy();
-    const json = await response.json()
-    expect(json).not.toHaveProperty("errors");
-  });
+
+    test("uploader can see the API listing", async ({ request }) => {
+        const response = await request.get(API_ENDPOINT, {
+            headers: {
+                Authorization: `Basic ${btoa("uploader:testpass")}`,
+            },
+        });
+        expect(response.ok()).toBeTruthy();
+        const json = await response.json();
+        expect(json).not.toHaveProperty("errors");
+    });
 });
