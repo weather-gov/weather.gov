727cac34217dcb04bed0014b262c026506aec910
Greg Walker <michael.walker@noaa.gov>
2024-09-03 13:11:37 -0500

    spruce up hourly temp

--- web/themes/new_weather_theme/assets/js/charts/hourly-temperature.js
+++ web/themes/new_weather_theme/assets/js/charts/hourly-temperature.js
@@ -1,5 +1,16 @@
 /* global Chart ChartDataLabels */
 (() => {
+  const fontMono = `DM Mono, "Roboto Mono Web", "Courier New", monospace, serif`;
+
+  const colors = {
+    base: "#71767A",
+    baseLighter: "#DFE1E2",
+    baseLightest: "#F5F6F7",
+    primary: "#005EA2",
+    primaryDark: "#0B4778",
+    primaryLight: "#0085CA",
+  };
+
   Chart.register(ChartDataLabels);
 
   const chartContainers = Array.from(
@@ -15,7 +26,17 @@
       Number.parseInt(v, 10),
     );
 
-    const chart = new Chart(container.querySelector("canvas"), {
+    // These are applied globally to all charts. Unclear if that's okay, or if
+    // what we really want is to set them per-chart, but this is what I've got
+    // for now.
+    Chart.defaults.font.family = fontMono;
+    Chart.defaults.font.size = `10px`;
+
+    // We don't need to keep a reference to the chart object. We only need the
+    // side-effects of creating it. This is not ideal, but it's how Chart.js
+    // works, so it's what we've got.
+    // eslint-disable-next-line no-new
+    new Chart(container.querySelector("canvas"), {
       type: "line",
       plugins: [ChartDataLabels],
 
@@ -31,7 +52,10 @@
         },
         scales: {
           x: {
-            ticks: { maxRotation: 0 },
+            ticks: {
+              maxRotation: 0,
+              color: colors.base,
+            },
             grid: {
               color: times.map((v) => {
                 if (v === "12 AM") {
@@ -40,27 +64,28 @@
 
                 const even = Number.parseInt(v, 10) % 2 === 0;
                 if (even) {
-                  return "#ccc";
+                  return colors.baseLighter;
                 }
-                return "#eee";
+                return colors.baseLightest;
               }),
             },
           },
           y: {
             min: Math.min(
-              0,
-              Math.min(...temps) - 15,
-              Math.min(...feelsLike) - 15,
+              // 0,
+              Math.round(Math.min(...temps) / 10) * 10 - 10,
+              Math.round(Math.min(...feelsLike) / 10) * 10 - 10,
             ),
             max: Math.max(
-              100,
-              Math.max(...temps) + 15,
-              Math.max(...feelsLike) + 15,
+              // 100,
+              Math.round(Math.max(...temps) / 10) * 10 + 10,
+              Math.round(Math.max(...feelsLike) / 10) * 10 + 10,
             ),
             ticks: {
               autoSkip: true,
+              color: colors.base,
               maxTicksLimit: 6,
-              callback: (v) => `${v}℉`,
+              callback: (v) => `${v}°`,
             },
           },
         },
@@ -74,15 +99,23 @@
             data: temps,
             datalabels: {
               align: (_, i) => (temps[i] > feelsLike[i] ? "top" : "bottom"),
+              color: colors.primaryDark,
             },
+            backgroundColor: colors.primaryDark,
+            borderColor: colors.primaryDark,
+            borderWidth: 1.5,
           },
           {
             label: "Feels like",
             data: feelsLike,
-            borderDash: [4],
             datalabels: {
               align: (_, i) => (temps[i] > feelsLike[i] ? "bottom" : "top"),
+              color: colors.primary,
             },
+            borderDash: [4],
+            backgroundColor: colors.primaryLight,
+            borderColor: colors.primaryLight,
+            borderWidth: 1.5,
           },
         ],
       },
