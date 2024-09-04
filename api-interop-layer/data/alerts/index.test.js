436e36c723b7a6245b2dda4f410df93c52942c3d
Greg Walker <michael.walker@noaa.gov>
2024-09-04 18:01:42 -0500

    proper test

--- api-interop-layer/data/alerts/index.test.js
+++ api-interop-layer/data/alerts/index.test.js
@@ -27,4 +27,77 @@ describe("alert data module", () => {
       fetch.calledWith("https://api.weather.gov/alerts/active?status=actual"),
     ).to.be.true;
   });
+
+  describe("after initial setup", () => {
+    let alertHandler;
+    before(async () => {
+      alertHandler = await import("./index.js");
+    });
+
+    describe("the main alert function", () => {
+      it("does not store alerts that are not land-based", async () => {
+        const shared = {
+          sent: new Date().toISOString(),
+          effective: new Date().toISOString(),
+          onset: new Date().toISOString(),
+          expires: new Date().toISOString(),
+          onset: new Date().toISOString(),
+        };
+
+        response.json.resolves({
+          features: [
+            {
+              geometry: "geo",
+              properties: {
+                id: "one",
+                event: "Severe Thunderstorm Warning",
+                ...shared,
+              },
+            },
+            {
+              geometry: "geo",
+              properties: {
+                id: "two",
+                event: "Special Marine Warning",
+                ...shared,
+              },
+            },
+            {
+              geometry: "geo",
+              properties: {
+                id: "three",
+                event: "Typhoon Warning",
+                ...shared,
+              },
+            },
+            {
+              geometry: "geo",
+              properties: {
+                id: "four",
+                event: "avalanche warning",
+                ...shared,
+              },
+            },
+          ],
+        });
+
+        // This is the doohickey that actually parses, filters, and sorts the
+        // alerts. We need to run it first.
+        await alertHandler.updateAlerts();
+
+        // Then the default export will find the applicable alerts for us, but it
+        // will do some database queries to find geospatial intersections. Gotta
+        // mock that too.
+        db.query.resolves([{ yes: 1 }]);
+
+        const alerts = await alertHandler.default({
+          grid: { geometry: [] },
+          place: { timezone: "America/Chicago" },
+        });
+
+        const kinds = alerts.items.map(({ metadata: { kind } }) => kind);
+        expect(kinds).to.have.same.members(["land", "land", "land"]);
+      });
+    });
+  });
 });
