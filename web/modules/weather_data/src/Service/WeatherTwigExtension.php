eca3199709c1da0ab8c9cfc2f2d5316cf6e3a4cb
eric-gade <eric.gade@gsa.gov>
2024-09-20 12:41:17 -0400

    Adding twig filter for AK wfo normalization
    Also updating places it should be used

---
+++ web/modules/weather_data/src/Service/WeatherTwigExtension.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Drupal\weather_data\Service;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+use Twig\TwigFunction;
+use Twig\Extension\ExtensionInterface;
+
+class WeatherTwigExtension extends AbstractExtension
+{
+    
+    public function getFunctions(){
+        return [
+            new TwigFunction(
+                "normalize_wfo",
+                [$this, 'normalizeWFO'],
+                ["is_safe" => ["html"]])
+        ];
+    }
+
+    public function getFilters(){
+        return [
+            new TwigFilter(
+                "normalize_wfo",
+                [$this, "normalizeWFO"]
+            )
+        ];
+    }
+
+    /**
+     * Handle the edge cases of WFO codes
+     * for Alaska/Anchorage
+     */
+    public function normalizeWFO(string $wfo)
+    {
+        $anchorageAlternates = ["alu", "aer"];
+        $isAlternate = in_array(
+            strtolower($wfo),
+            $anchorageAlternates
+        );
+        if($isAlternate){
+            return "AFC";
+        }
+
+        return $wfo;
+    }
+};
