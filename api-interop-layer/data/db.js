9d6f34587534d3e6ac20485aa9c73d05714e8a91
James Tranovich <james.tranovich@noaa.gov>
2024-09-25 15:41:01 -0700

    interop deploy: get creds from cloud.gov

--- api-interop-layer/data/db.js
+++ api-interop-layer/data/db.js
@@ -2,16 +2,35 @@ import mariadb from "mariadb";
 
 import { sleep } from "../util/sleep.js";
 
-export const openDatabase = async () => {
-  const connectionDetails = {
-    user: process.env.DB_USERNAME ?? "drupal",
-    password: process.env.DB_PASSWORD ?? "drupal",
-    database: process.env.DB_NAME ?? "weathergov",
-    host: process.env.DB_HOST ?? "database",
-    port: process.env.DB_PORT ?? 3306,
-    ssl: { rejectUnauthorized: false },
-  };
+const getDatabaseConnection = () => {
+  if (process.env.PRODUCTION) {
+    // we are in a cloud.gov environment and must retrieve credentials from
+    // the VCAP_SERVICES environment variable
+    const vcap = JSON.parse(process.env.VCAP_SERVICES);
+    const db = vcap["aws-rds"][0];
+    return {
+      user: db.credentials.username,
+      password: db.credentials.password,
+      database: db.credentials.name,
+      host: db.credentials.host,
+      port: db.credentials.port,
+    };
+    return mariadb.createConnection(connectionDetails);
+  } else {
+    // we are in a local environment: offer defaults for ease of use
+    return {
+      user: process.env.DB_USERNAME ?? "drupal",
+      password: process.env.DB_PASSWORD ?? "drupal",
+      database: process.env.DB_NAME ?? "weathergov",
+      host: process.env.DB_HOST ?? "database",
+      port: process.env.DB_PORT ?? 3306,
+      ssl: { rejectUnauthorized: false },
+    };
+  }
+}
 
+export const openDatabase = async () => {
+  const connectionDetails = getDatabaseConnection();
   return mariadb.createConnection(connectionDetails);
 };
 
