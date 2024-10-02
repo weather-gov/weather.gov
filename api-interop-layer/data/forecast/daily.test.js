1ded42996d5b90362383f51de0260235a816ed0f
James Tranovich <james.tranovich@noaa.gov>
2024-10-02 09:01:17 -0700

    interop tests: add documentation and tests

--- api-interop-layer/data/forecast/daily.test.js
+++ api-interop-layer/data/forecast/daily.test.js
@@ -7,10 +7,11 @@ import path from "path";
 
 describe("daily forecast", () => {
 
-  // timezone is only used to calculate monthAndDay/dayName information
+  // timezone is only used to calculate monthAndDay/dayName information and is
+  // not important for this test suite
   const timezone = "America/New_York";
 
-  it("returns one overnight period when between midnight and 6am", () => {
+  it("returns one day when given one overnight period", () => {
     const data = {
       properties: {
         periods: [{
@@ -20,8 +21,9 @@ describe("daily forecast", () => {
         }],
       },
     };
-    const dailyData = daily(data, { timezone });
-    const [ firstDay ] = dailyData.days;
+    const { days } = daily(data, { timezone });
+    expect(days.length).to.equal(1);
+    const [ firstDay ] = days;
     expect(firstDay.periods.length).to.equal(1);
     const [ firstPeriod ] = firstDay.periods;
     expect(firstPeriod.isDaytime).not.to.be.true;
@@ -30,53 +32,105 @@ describe("daily forecast", () => {
     expect(firstPeriod.dayName).to.equal("Today");
   });
 
-  it("returns one overnight period when between midnight and 6am, even with data before midnight", () => {
+  it("returns one day when given two periods", () => {
     const data = {
       properties: {
         periods: [{
-          startTime: "2024-08-31T11:00:00-10:00",
-          endTime: "2024-09-01T01:00:00-10:00",
+          startTime: "2024-09-01T01:00:00-10:00",
+          endTime: "2024-09-01T02:00:00-10:00",
+          isDaytime: true,
+        },
+        {
+          startTime: "2024-09-01T13:00:00-10:00",
+          endTime: "2024-09-01T15:00:00-10:00",
           isDaytime: false,
         }],
       },
     };
-    const dailyData = daily(data, { timezone });
-    const [ firstDay ] = dailyData.days;
-    expect(firstDay.periods.length).to.equal(1);
-    const [ firstPeriod ] = firstDay.periods;
-    expect(firstPeriod.isDaytime).not.to.be.true;
-    expect(firstPeriod.isOvernight).to.be.true;
-    expect(firstPeriod.timeLabel).to.equal("NOW-6AM");
-    expect(firstPeriod.dayName).to.equal("Today");
-  });
-
-  it("returns one tonight period when between 6pm and midnight", () => {
-    const data = {
-      properties: {
-        periods: [{
-          startTime: "2024-05-06T23:00:00-05:00",
-          endTime: "2024-05-06T23:59:00-0500",
-          isDaytime: false,
-        }],
-      },
-    };
-    const dailyData = daily(data, { timezone });
-    const [ firstDay ] = dailyData.days;
-    expect(firstDay.periods.length).to.equal(1);
-    const [ firstPeriod ] = firstDay.periods;
-        console.log(JSON.stringify(firstPeriod));
-
-    expect(firstPeriod.isDaytime).not.to.be.true;
+    const { days } = daily(data, { timezone });
+    expect(days.length).to.equal(1);
+    const [ firstDay ] = days;
+    expect(firstDay.periods.length).to.equal(2);
+    const [ firstPeriod, secondPeriod ] = firstDay.periods;
+    expect(firstPeriod.isDaytime).to.be.true;
     expect(firstPeriod.isOvernight).not.to.be.true;
-    expect(firstPeriod.timeLabel).to.equal("6PM-6AM");
+    expect(firstPeriod.timeLabel).to.equal("6AM-6PM");
+    expect(firstPeriod.dayName).to.equal("Today");
+    expect(secondPeriod.isDaytime).not.to.be.true;
+    expect(secondPeriod.isOvernight).not.to.be.true;
+    expect(secondPeriod.timeLabel).to.equal("6PM-6AM");
+    expect(secondPeriod.dayName).to.equal("Today");
+  });
+
+  it("returns two days when given three periods", () => {
+    const data = {
+      properties: {
+        periods: [{
+          startTime: "2024-09-01T01:00:00-10:00",
+          endTime: "2024-09-01T02:00:00-10:00",
+          isDaytime: true,
+        },
+        {
+          startTime: "2024-09-01T13:00:00-10:00",
+          endTime: "2024-09-01T15:00:00-10:00",
+          isDaytime: false,
+        },
+        {
+          startTime: "2024-09-02T00:00:00-10:00",
+          endTime: "2024-09-02T06:00:00-10:00",
+          isDaytime: true,
+        }],
+      },
+    };
+    const { days } = daily(data, { timezone });
+    expect(days.length).to.equal(2);
+    const [ firstDay, secondDay ] = days;
+    expect(firstDay.periods.length).to.equal(2);
+    const [ firstPeriod, secondPeriod ] = firstDay.periods;
+    expect(firstPeriod.isDaytime).to.be.true;
+    expect(firstPeriod.isOvernight).not.to.be.true;
+    expect(firstPeriod.timeLabel).to.equal("6AM-6PM");
+    expect(firstPeriod.dayName).to.equal("Today");
+    expect(secondPeriod.isDaytime).not.to.be.true;
+    expect(secondPeriod.isOvernight).not.to.be.true;
+    expect(secondPeriod.timeLabel).to.equal("6PM-6AM");
+    expect(secondPeriod.dayName).to.equal("Today");
+    expect(secondDay.periods.length).to.equal(1);
+  });
+
+  it("accepts periods with timezones of z", () => {
+    const data = {
+      properties: {
+        periods: [{
+          startTime: "2024-05-06T23:00:00Z",
+          endTime: "2024-05-06T23:59:00Z",
+          isDaytime: false,
+        }],
+      },
+    };
+    const { days } = daily(data, { timezone });
+    expect(days.length).to.equal(1);
+    const [ firstDay ] = days;
+    expect(firstDay.periods.length).to.equal(1);
+    const [ firstPeriod ] = firstDay.periods;
+    expect(firstPeriod.isDaytime).not.to.be.true;
+    expect(firstPeriod.isOvernight).to.be.true;
+    expect(firstPeriod.timeLabel).to.equal("NOW-6AM");
     expect(firstPeriod.dayName).to.equal("Today");
   });
 
-  it("returns no overnight period when between 6am and midnight", () => {
-    // TODO
-  });
-
-  it("returns three periods when between midnight and 6am", () => {
-    // TODO
+  it("propagates an error", () => {
+    const data = {
+      error: true,
+      properties: {
+        periods: [{
+          startTime: "2024-05-06T23:00:00+04:00",
+          endTime: "2024-05-06T23:59:00+01:00",
+          isDaytime: false,
+        }],
+      },
+    };
+    const dailyData = daily(data, { timezone });
+    expect(dailyData.error).to.be.true;
   });
 });
