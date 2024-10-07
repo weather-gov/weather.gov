9d86402609d7fd7e7fb5e6c71060a65bbc7434f1
Greg Walker <michael.walker@noaa.gov>
2024-10-07 13:15:09 -0500

    let chart.js figure out the x axis ticks

--- web/themes/new_weather_theme/assets/js/charts/hourly-pops.js
+++ web/themes/new_weather_theme/assets/js/charts/hourly-pops.js
@@ -60,7 +60,7 @@ for (const container of chartContainers) {
     },
 
     data: {
-      labels: times.map((v) => (Number.parseInt(v, 10) % 2 === 0 ? v : "")),
+      labels: times,
       datasets: [
         {
           label: "Chance of precipitation",
