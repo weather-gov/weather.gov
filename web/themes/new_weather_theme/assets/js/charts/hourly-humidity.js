5140445ba4aba5bfa644aae83fdded9406ad7c63
Greg Walker <142943695+greg-does-weather@users.noreply.github.com>
2024-10-07 16:52:08 -0500

    let chart.js figure out the x axis ticks (#1868)

--- web/themes/new_weather_theme/assets/js/charts/hourly-humidity.js
+++ web/themes/new_weather_theme/assets/js/charts/hourly-humidity.js
@@ -65,7 +65,7 @@ for (const container of chartContainers) {
     },
 
     data: {
-      labels: times.map((v) => (Number.parseInt(v, 10) % 2 === 0 ? v : "")),
+      labels: times,
       datasets: [
         {
           label: "Humidity",
