1f2dea8f0c71dca349aae6affb01fa0ba9a9c0e6
Greg Walker <michael.walker@noaa.gov>
2024-10-23 13:47:08 -0500

    split out monitoring components; add tests

---
+++ api-interop-layer/util/monitoring/newRelic.js
@@ -0,0 +1,36 @@
+import newrelic from "newrelic";
+
+const NEW_RELIC_METRICS_URL = "https://metric-api.newrelic.com/metric/v1";
+const [appName] = newrelic.agent?.config.app_name ?? [];
+const licenseKey = newrelic.agent?.config.license_key;
+
+let sendMetric = (metric) => {
+  // augment metric attributes with our application name, since NR metrics does
+  // not record where we are sending this metric from
+  metric.attributes ??= {};
+  metric.attributes.applicationName = appName;
+  metric.timestamp ??= Date.now();
+
+  const body = JSON.stringify([{ metrics: [metric] }]);
+  return fetch(NEW_RELIC_METRICS_URL, {
+    method: "POST",
+    headers: { "Api-Key": licenseKey },
+    body,
+  }).then(async (r) => {
+    const response = await r.json();
+    if (r.status !== 202) {
+      console.log(`NR error: ${response}`); // eslint-disable-line no-console
+    }
+    return response;
+  });
+};
+
+let recordEvent = newrelic.recordLogEvent;
+
+if (!licenseKey) {
+  sendMetric = () => {};
+  recordEvent = () => {};
+}
+
+export const sendNewRelicMetric = sendMetric;
+export const recordLogEvent = recordEvent;
