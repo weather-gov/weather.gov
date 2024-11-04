a13e78092023640607ff854bf3dc0cb1b6819fbf
eric-gade <eric.gade@gsa.gov>
2024-11-04 14:38:44 -0500

    Moar lints

--- api-interop-layer/data/index.js
+++ api-interop-layer/data/index.js
@@ -4,7 +4,7 @@ import getForecast from "./forecast/index.js";
 import getObservations from "./obs/index.js";
 import getPoint from "./points.js";
 import getSatellite from "./satellite.js";
-import getProduct from "./products/index.js";
+import getProductById from "./products/index.js";
 import { createLogger } from "../util/monitoring/index.js";
 
 const logger = createLogger("forecast");
@@ -77,7 +77,6 @@ export const getDataForPoint = async (lat, lon) => {
   };
 };
 
-export {
-  getDataForPoint as default,
-  getProduct as getProductById
-};
+export default getDataForPoint;
+export getProductById;
+
