9674db9d1ad6d81e6c21f249dae9c80cfb027f44
James Tranovich <james.tranovich@noaa.gov>
2024-11-15 12:19:25 -0800

    weather story: retrieve from derived wfo

--- web/modules/weather_data/src/Service/WeatherEntityService.php
+++ web/modules/weather_data/src/Service/WeatherEntityService.php
@@ -101,7 +101,7 @@ class WeatherEntityService
             ->accessCheck(false)
             ->condition("status", 1)
             ->condition("type", $nodeType)
-            ->condition("field_office", $wfo)
+            ->condition("field_derived_wfo", $wfo)
             ->sort("changed", "DESC")
             // Only get the first one.
             ->range(0, 1)
