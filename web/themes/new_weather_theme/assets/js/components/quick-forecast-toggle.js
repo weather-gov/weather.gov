69c4285f186f0c0b3772f0ad740bbc228ad2777b
eric-gade <eric.gade@gsa.gov>
2024-11-18 15:35:42 -0500

    Wrapping up tablet/mobile quick forecast accordion views

--- web/themes/new_weather_theme/assets/js/components/quick-forecast-toggle.js
+++ web/themes/new_weather_theme/assets/js/components/quick-forecast-toggle.js
@@ -2,8 +2,21 @@
 // of a daily forecast list item in non-desktop views
 const clickHandler = (event) => {
   event.preventDefault();
+  event.stopPropagation();
+  console.log(event);
   const isExpanded = event.currentTarget.getAttribute("data-expanded") === "true";
   event.currentTarget.setAttribute("data-expanded", !isExpanded);
+  Array.from(
+    event.currentTarget.querySelectorAll(".wx-toggler-add-icon use")
+  ).forEach(element => {
+    const iconName = isExpanded ? "add_circle" : "remove_circle";
+    const iconHref = element.getAttribute("xlink:href").split("#")[0];
+    element.setAttributeNS(
+      "http://www.w3.org/1999/xlink",
+      "xlink:href",
+      `${iconHref}#${iconName}`,
+    );
+  });
 };
 
 Array.from(
