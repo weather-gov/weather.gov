ec9ef145a8de670c8ce6d0bd3d58d7836b3d1f1d
James Tranovich <james.tranovich@noaa.gov>
2024-11-26 11:17:02 -0800

    pm2: move inspect/gc to dev

--- api-interop-layer/ecosystem.config.cjs
+++ api-interop-layer/ecosystem.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
     script: "main.js",
     ignore_watch: ["newrelic_agent.log"],
     args: "--update-env",
-    interpreter_args: "--inspect=0.0.0.0:9229 --expose-gc --experimental-loader newrelic/esm-loader.mjs -r newrelic",
+    interpreter_args: "--experimental-loader newrelic/esm-loader.mjs -r newrelic",
     watch: true
   }]
 }
