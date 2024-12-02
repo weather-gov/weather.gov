746df41cea74f9d15659f1d0c99b42c078febc7a
James Tranovich <james.tranovich@noaa.gov>
2024-12-02 14:46:09 -0800

    pm2 configuration tweaks

--- api-interop-layer/ecosystem.config.cjs
+++ api-interop-layer/ecosystem.config.cjs
@@ -2,9 +2,9 @@ module.exports = {
   apps: [{
     name: "api-interop-layer",
     script: "main.js",
-    ignore_watch: ["newrelic_agent.log"],
+    ignore_watch: ["newrelic_agent.log", "node_modules", ".pm2"],
     args: "--update-env",
     interpreter_args: "--experimental-loader newrelic/esm-loader.mjs -r newrelic",
-    watch: true
+    watch: ["*.js"]
   }]
 }
