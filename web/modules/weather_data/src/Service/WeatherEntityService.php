216955d25bb430deea96550daafec1878915d607
James Tranovich <james.tranovich@noaa.gov>
2024-12-20 14:26:34 -0800

    dss builder: bugfix to accommodate 4 char WFO ids

--- web/modules/weather_data/src/Service/WeatherEntityService.php
+++ web/modules/weather_data/src/Service/WeatherEntityService.php
@@ -94,6 +94,8 @@ class WeatherEntityService
 
     public function getLatestWeatherStoryImageFromWFO($wfo, $nodeType)
     {
+        // we get canonical four letters from the DSS builder. omit the first character.
+        $truncatedWFO = substr($wfo, 1);
         // get the latest weather story upload that matches the grid WFO.
         $nodeID = $this->entityTypeManager
             ->getStorage("node")
@@ -101,7 +103,7 @@ class WeatherEntityService
             ->accessCheck(false)
             ->condition("status", 1)
             ->condition("type", $nodeType)
-            ->condition("field_office", $wfo)
+            ->condition("field_office", $truncatedWFO)
             ->sort("changed", "DESC")
             // Only get the first one.
             ->range(0, 1)
