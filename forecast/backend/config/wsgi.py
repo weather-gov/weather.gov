24dd96dc9ffa5077d0062bfbd78b7d888118928e
Seamus Johnston <seamus.johnston@gsa.gov>
2025-02-06 15:37:08 -0600

    Consolidate Django code under forecast dir

--- forecast/backend/config/wsgi.py
+++ forecast/backend/config/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "weather.config.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.config.settings")
 
 application = get_wsgi_application()
