348e8eccdd20e5cb9df7a318f4312deebd331ada
eric-gade <eric.gade@gsa.gov>
2025-02-20 08:50:37 -0500

    Merging

--- forecast/backend/urls.py
+++ forecast/backend/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path, register_converter
-
+from django.urls import path, include
+from wagtail.admin import urls as wagtailadmin_urls
+from wagtail import urls as wagtail_urls
+from wagtail.documents import urls as wagtaildocs_urls
 from . import views
 from .url_converters import FloatConverter
 
@@ -7,7 +10,6 @@ register_converter(FloatConverter, "float")
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("point/<float:lat>/<float:lon>", views.point_location, name="point"),
     path("offices", views.offices, name="offices"),
     path("afd/", views.afd_index, name="afd_index"),
     path("afd/<wfo>", views.afd_by_office, name="afd_by_office"),
@@ -17,4 +19,9 @@ urlpatterns = [
     # that will be requested from the frontend (htmx style)
     path("wx/afd/<afd_id>", views.wx_afd_id, name="wx_afd_id"),
     path("wx/afd/locations/<wfo>", views.wx_afd_versions, name="wx_afd_versions"),
+
+    path('cms/', include(wagtailadmin_urls)),
+    path('documents/', include(wagtaildocs_urls)),
+    path('pages/', include(wagtail_urls)),
+    path("point/<float:lat>/<float:lon>", views.point_location, name="point"),
 ]
