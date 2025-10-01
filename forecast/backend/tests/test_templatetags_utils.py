d594d6cbcd17ab19325493cac5111fedcc1ff27c
Greg  Walker <greg.m.walker@noaa.gov>
2025-10-01 20:49:25 +0000

    Add tests for our template tag utilities
    Changelog: patch

---
+++ forecast/backend/tests/test_templatetags_utils.py
@@ -0,0 +1,61 @@
+import json
+
+from django.test import TestCase
+
+from backend.templatetags import util
+
+
+class TestTemplateTagUtilities(TestCase):
+    """Test template tag utilities."""
+
+    def test_subtract_with_first_empty(self):
+        """Tests that an empty string is coerced to zero."""
+        self.assertEquals(util.subtract("", 7), -7)
+
+    def test_subtract_with_second_empty(self):
+        """Tests that an empty string is coerced to zero."""
+        self.assertEquals(util.subtract(9, ""), 9)
+
+    def test_subtract(self):
+        """Tests that a strings are coerced to floats."""
+        self.assertEquals(util.subtract("13", 4), 9)
+
+    def test_place_label_with_no_state(self):
+        """Tests that we don't include a state if there's not one."""
+        self.assertEquals(util.place_label({"name": "Townsville"}), "Townsville")
+
+    def test_place_label_with_state(self):
+        """Tests that we include state if present."""
+        self.assertEquals(
+            util.place_label({"name": "Townsville", "state": "PL"}),
+            "Townsville, PL",
+        )
+
+    def test_json_encode_with_bad_type(self):
+        """Tests that JSON encoding anything other than a dict or list results in an empty string."""
+        self.assertEquals(util.json_encode("They're coming to get you, Barbara"), "")
+
+    def test_json_encode(self):
+        """Tests that JSON encoding... encodes."""
+        # Use the json.dumps method here to get our expected output. Don't hard
+        # code the expected string as that relies on the internal behavior of
+        # json.dumps, which could change.
+        o = {"key": "value"}
+        self.assertEquals(util.json_encode(o), json.dumps(o))
+
+    def test_normalize_wfo_alu(self):
+        """Tests that ALU is normalized to Anchorage."""
+        self.assertEquals(util.normalize_wfo("ALU"), "AFC")
+
+    def test_normalize_wfo_aer(self):
+        """Tests that AER is normalized to Anchorage."""
+        self.assertEquals(util.normalize_wfo("AER"), "AFC")
+
+    def test_normalize_wfo_others(self):
+        """Tests that other WFO codes are retained."""
+        self.assertEquals(util.normalize_wfo("ABC"), "ABC")
+
+    def test_normalize_alert_whitespace(self):
+        """Test alert whitespace normalization."""
+        actual = util.normalize_alert_whitespace("This is\nsome text\nin an\nalert")
+        self.assertEquals(actual, "This is<br />some text<br />in an<br />alert")
