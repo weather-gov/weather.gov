bb77a9d771774e4f0141bb324abcb89b5059f8c8
James Tranovich <james.tranovich@noaa.gov>
2025-11-10 08:31:18 -0800

    bugfix: pass in proper env var names for new relic
    Changelog: minor

--- api-interop-layer/newrelic.cjs
+++ api-interop-layer/newrelic.cjs
@@ -13,7 +13,7 @@ exports.config = {
   /**
    * Your New Relic license key.
    */
-  license_key: process.env.NEWRELIC_LICENSE,
+  license_key: process.env.NEW_RELIC_LICENSE_KEY,
   logging: {
     /**
      * Level at which to log. 'trace' is most useful to New Relic when diagnosing
