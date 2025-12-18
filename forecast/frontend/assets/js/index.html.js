b4a70f1eafc55081eef0e5e583b41c483ef67560
Eric Gade <eric.gade@noaa.gov>
2025-12-18 15:36:30 +0000

    Re-nabling old combobox on front page
    Changelog: minor

--- forecast/frontend/assets/js/index.html.js
+++ forecast/frontend/assets/js/index.html.js
@@ -1,5 +1,3 @@
 import "./locationSearch.js";
-// import "./components/combo-box.js";
-// import "./components/combo-box-location.js";
-import "./components/combobox/combobox.js";
-import "./components/combobox/location-listbox.js";
+import "./components/combo-box.js";
+import "./components/combo-box-location.js";
