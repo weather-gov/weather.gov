94d946b921d61eb60e1f27ec01f9e2cc2f5b35d2
Greg  Walker <greg.m.walker@noaa.gov>
2025-12-23 18:22:00 +0000

    Overhaul how we get risk overviews from upstream
    Changelog: minor

--- api-interop-layer/util/fetch.js
+++ api-interop-layer/util/fetch.js
@@ -5,22 +5,35 @@ const logger = createLogger("fetch wrapper");
 
 const BASE_URL = process.env.API_URL ?? "https://api.weather.gov";
 const BASE_GHWO_URL = process.env.GHWO_URL ?? "https://www.weather.gov";
-const headers = process.env.API_KEY ? { "API-Key": process.env.API_KEY } : {};
+const STANDARD_HEADERS = process.env.API_KEY
+  ? { "API-Key": process.env.API_KEY }
+  : {};
 
 const internalFetch = async (path) => {
-  let url = URL.canParse(path) ? path : new URL(path, BASE_URL).toString();
+  let url = URL.canParse(path)
+    ? URL.parse(path)
+    : new URL(path, BASE_URL).toString();
 
-  // If the incoming path matches a request to
-  // the website's risk overview endpoint, let's try to proxy the
-  // request if possible
-  if (path.endsWith("hazByCounty.json")) {
-    const hazardOutlookUrl = URL.parse(path);
-    logger.verbose(`hazard outlook request to: ${hazardOutlookUrl.pathname}`);
-    url = new URL(hazardOutlookUrl.pathname, BASE_GHWO_URL).toString();
+  const headers = { ...STANDARD_HEADERS };
+
+  // If the hostname is specified in the original request, preserve that as
+  // a special header. In dev environments, our magic proxy will pick this up
+  // to use for routing.
+  if (url.hostname) {
+    headers["wx-host"] = url.hostname;
+  }
+
+  if (
+    url.hostname === "www.weather.gov" &&
+    url.pathname.startsWith("/source/")
+  ) {
+    // If the incoming path matches a request to the website's risk overview
+    // endpoint, switch to the GHWO base URL.
+    url = new URL(url.pathname, BASE_GHWO_URL);
   }
   logger.verbose(`making request to ${url}`);
 
-  return fetch(url, headers).then(async (r) => {
+  return fetch(url, { headers }).then(async (r) => {
     // If there are headers, get the correlation ID. There may not be one, but
     // that's beside the point. We'll attach the correlation ID to downstream
     // log messages about the success/failure of this response.
