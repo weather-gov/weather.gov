01ba8be82ea106a0dfb88e19572c29ad9e70054e
Eric Gade <eric.gade@noaa.gov>
2026-01-27 18:00:52 +0000

    Adding RUM to the front page bundle
    Changelog: minor

--- forecast/frontend/assets/js/front-page.html.js
+++ forecast/frontend/assets/js/front-page.html.js
@@ -2,6 +2,9 @@
 import "./locationSearch.js";
 import "./components/combo-box-location.js";
 
+// RUM
+import "./aws-rum.js";
+
 // From base.html.js
 import "./components/timer.js";
 import "./components/aria-live.js";
