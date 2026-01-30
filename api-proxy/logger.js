17d400ac09b92ebee255c360d779d53e2b978455
James Tranovich <james.tranovich@noaa.gov>
2026-01-30 10:13:54 -0800

    Make node logs pretty
    Changelog: minor

--- api-proxy/logger.js
+++ api-proxy/logger.js
@@ -1,17 +1,26 @@
 import pino from "pino";
 
 const transport = pino.transport({
+  serializers: {
+    req: pino.stdSerializers.req,
+    res: pino.stdSerializers.res,
+  },
+  // api-proxy will always be for dev purposes.
+  // so, log trace and above to stdout (fd 1)
   targets: [
-    // api-proxy will always be for dev purposes.
-    // so, log trace and above to stdout (fd 1)
     {
+      target: "pino-pretty",
+      options: { colorize: true },
       level: process.env.LOG_LEVEL || "trace",
-      target: "pino/file",
-      options: { destination: 1 },
     },
   ],
 });
 
-const logger = pino(transport);
+const logger = pino(
+  {
+    level: process.env.LOG_LEVEL || "trace",
+  },
+  transport,
+);
 
 export default logger;
