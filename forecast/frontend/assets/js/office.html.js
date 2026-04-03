cc970dab0a37a86176788c01ae41214412c33377
Igor Korenfeld <igor.korenfeld@noaa.gov>
2026-04-03 18:45:01 +0000

    Update WFO page content and layout
    Changelog: minor

---
+++ forecast/frontend/assets/js/office.html.js
@@ -0,0 +1,7 @@
+/**
+ * @file JavaScript for the weather forecasting office (WFO) page.
+ * Used by: office/overview.html
+ */
+
+// Nota bene: JS used on every page goes in page.html.js.
+import "./page.html.js";
