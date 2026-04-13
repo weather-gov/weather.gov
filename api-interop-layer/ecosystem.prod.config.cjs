eca5108dbf7783f86193dd5a1a1dac3fd9dd1597
James Tranovich <james.tranovich@noaa.gov>
2026-04-13 12:00:54 -0700

    adjust pool calculations based on pm2 clustering
    Changelog: minor

--- api-interop-layer/ecosystem.prod.config.cjs
+++ api-interop-layer/ecosystem.prod.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   apps: [{
     name: "api-interop-layer",
     script: "main.js",
-    instances: "2",
+    instances: process.env.API_NODE_APPS || "2",
     exec_mode: "cluster",
     ignore_watch: ["newrelic_agent.log", "node_modules", ".pm2"],
     args: "--update-env",
