eed673be22e082a1d7b0beb241a088423d20e9ce
James Tranovich <james.tranovich@noaa.gov>
2026-05-26 11:36:33 -0700

    enable new relic for staging
    Changelog: minor

--- api-interop-layer/ecosystem.dev.config.cjs
+++ api-interop-layer/ecosystem.dev.config.cjs
@@ -1,10 +1,11 @@
+const enableNewRelic = !!process.env.NEW_RELIC_LICENSE_KEY;
 module.exports = {
   apps: [{
     name: "api-interop-layer",
     script: "main.js",
     ignore_watch: ["newrelic_agent.log", "node_modules", ".pm2"],
     args: "--update-env",
-    interpreter_args: "--experimental-loader newrelic/esm-loader.mjs -r newrelic",
+    interpreter_args: enableNewRelic ? "--experimental-loader newrelic/esm-loader.mjs -r newrelic" : "",
     watch: ["**/*.js"],
     node_args: "--import ./tracing.js --inspect=0.0.0.0:9229 --expose-gc"
   }]
